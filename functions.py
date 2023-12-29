2de7839472373f397038a46d362c51db567408b1
mekkilila <mekkilila@yahoo.fr>
2023-12-29 17:54:39 +0100

    fichier functions propre et notebook actualisé

--- functions.py
+++ functions.py
@@ -9,13 +9,22 @@ from re import sub
 from unidecode import unidecode
 import spacy
 from scipy.spatial.distance import cosine
+import re 
+from unidecode import unidecode
+from conversions_unites import liquides_en_ml, solides_en_g
+from pattern.text.fr import singularize
+from fractions import Fraction
 
-## Fonctions d'affichage 
+##############################################################################################################################################
+
+## Fonctions d'affichage ##
+
 
 def transport_pie(dataframe, column, titre):
     """ 
-    Fonction qui prend en entrée un dataframe, une colonne spécifiée et le titre correspondant 
-    et qui renvoie la répartition correspondante 
+    Fonction qui prend en entrée un dataframe, une colonne relative aux moyens de transports et le titre correspondant.
+    Elle renvoie en sortie le diagramme correspondant à la répartition.
+
     """
     mapping_labels = {1: 'Voiture/Véhicule motorisé ', 2: 'Transport public', 3: 'A pied', 4: 'Vélo/Trottinette'}
     dataframe[column] = dataframe[column].map(mapping_labels)
@@ -28,6 +37,11 @@ def transport_pie(dataframe, column, titre):
     return None
 
 def pie_oui_non_eps(dataframe, column, titre):
+    """
+    Fonction qui prend en entrée un dataframe, une colonne dont les reponses possibles sont oui, non, quelques aures modalités, et un titre. 
+    Elle renvoie la répartition correspondante en sortie. 
+    
+    """
     mapping_eps = {1: 'Oui', 2: 'Non, dispensé', 3: 'Non, pas de cours prévu', 4: 'Ne sait pas', 5: 'Refus'}
     dataframe[column] = dataframe[column].map(mapping_eps)
     count_by_format = dataframe[column].value_counts()
@@ -39,6 +53,11 @@ def pie_oui_non_eps(dataframe, column, titre):
     return None
 
 def pie_oui_non(dataframe, column, titre):
+    """
+    Fonction qui prend en entrée un dataframe, une colonne dont les reponses possibles sont oui, non, et un titre. 
+    Elle renvoie la répartition correspondante en sortie. 
+    
+    """
     mapping_labels = {1: 'Oui', 2: 'Non'}
     dataframe[column] = dataframe[column].map(mapping_labels)
     count_by_format = dataframe[column].value_counts()
@@ -49,13 +68,25 @@ def pie_oui_non(dataframe, column, titre):
     plt.show()
     return None
 
+##############################################################################################################################################
 
-## Fonctions de nettoyage des données 
+## Fonctions de nettoyage des données ##
 
 nltk.download('stopwords')
 nltk.download('punkt')
 
 def cleaning(s):
+    """
+    Cette fonction a pour but de nettoyer une chaîne de caractère d'aliment, en prenant en compte des stopwords et des expressions 
+    régulières notamment. 
+    
+    Args : 
+        s (str): chaîne de caractère à convertir (elle peut être en majuscules, minuscules, avec ou sans accents...)
+    
+    Returns :
+        la chaîne de caractère "simplifiée" au maximum 
+
+    """
     from unidecode import unidecode 
     stop_words_spe = ['CRU','CRUE','ALIMENT','TOUT','TYPE','PREEMBALLE','PREEMBALLEE','PREEMBALLEES','MOYEN','CUIT',
                       'CUITE','PETIT DEJEUNER','ROTI','ROTIE','FOUR','AU FOUR','KG','CL','G','L','MG','MARTINIQUE',
@@ -68,19 +99,29 @@ def cleaning(s):
     s = sub("[^A-Z ]", " ", s)
     mots = word_tokenize(s)
     mots_filtres = [mot for mot in mots if mot not in stop_words]
-    #from nltk.stem.snowball import FrenchStemmer
-    #s = " ".join(FrenchStemmer().stem(s) for s in s.split())
     return ' '.join(mots_filtres)
 
-## Fonctions de distance et de matching 
+##############################################################################################################################################
 
-
-#!python -m spacy download fr_core_news_sm     # Téléchargement du modèle de traitement du français
+## Fonction de distance/matching  et conversions ## 
 
 nlp = spacy.load('fr_core_news_sm')    #Modèle de traitement prédéfini, avec distance
 
-
 def find_match(aliment_entre, dataframe, vec, seuil=0.8):
+    """
+    Cette fonction prend un aliment de la recette considérée en entrée (aliment_entre) ainsi qu'un dataframe 
+    (contentant les ingrédients dont on connaît les caractéristiques ), un vecteur (vec) et un seuil. Elle cherche dans la vectorisation des aliments 
+    de la base, l'aliment le plus proche de l'aliment de la recette en entrée. Si elle trouve une correspondance avec une distance supérieure au 
+    seuil fixé (souvent 0.8 en pratique), elle apparie cet ingrédient à notre ingrédient d'entrée. Sinon, elle renvoie qu'il n'y a pas de 
+    correspondance dans notre base. 
+
+    Args : 
+        aliment_entre (str) : aliment d'une recette, duquel on veut connaître les caractéristiques nutritionnelles grâce à la base de données 
+        dataframe (dataframe): contient les informations nutritionnelles et les aliments de la grande distribution
+        vec (vector) : vectorisation des aliments de la base de données grace à spacy et modèle de nlp 
+        seuil (float) : seuil de correspondance des aliments 
+
+    """
     # Vectoriser le nouvel aliment
     vecteur_aliment_entre = nlp(aliment_entre).vector
 
@@ -100,51 +141,13 @@ def find_match(aliment_entre, dataframe, vec, seuil=0.8):
         return dataframe['Nom clean'].iloc[index_correspondance]
     else:
         return "Aucune correspondance trouvée"
-
-
-
-
-def calcul_calories(recette,vec):                                     #Renvoie la valeur en kcal de la recette, cette dernière étant sous la forme : {'recette' : 'Titre recette' , 'Liste ingrédients' : [(ingrédient1, quantité1 en g);(ingrédient2, quantité2 en g) ...] , 'url':'url}#
-    valeur = 0
-    for ing in recette['Liste des ingrédients']:
-        nom_ingredient = ing[0]
-        nom_ingredient = nom_ingredient.upper()
-        quantite = ing[1]  # La quantité doit être en g, la bdd ciqual rapporte les apports pour 100g
-
-        # Utilisez le nom de l'ingrédient pour trouver la correspondance dans la base de données
-        nom_ciqual = find_match(nom_ingredient, data_ciqual,vec)
-
-        # Vérifiez si la correspondance a été trouvée
-        if nom_ciqual == "Aucune correspondance trouvée":
-            continue
-        else:
-            # Filtrer le DataFrame pour l'ingrédient spécifié
-            ligne_aliment = data_ciqual[data_ciqual['Nom clean'] == nom_ciqual]
-
-            # Vérifiez si la correspondance a été trouvée dans la base de données
-            if ligne_aliment.empty:
-                continue
-
-            energie_kcal_str = ligne_aliment['Energie kcal'].iloc[0].replace(',', '.')
-            
-            try:
-                energie_kcal = float(energie_kcal_str)
-            except ValueError:
-                print(f"Erreur de conversion pour l'ingrédient {nom_ingredient}, Energie kcal : {energie_kcal_str}")
-                continue
-
-            # Calculez les calories pour l'ingrédient
-            calorie_ingredient = energie_kcal * quantite / 100
-            valeur += calorie_ingredient
-
-    return valeur
-
-  
-## Conversion des strings 
     
-from fractions import Fraction
 
 def string_to_float(value):
+    """
+    Transforme la valeur value en flottant, utile pour les ingrédients des recettes, qui sont des str et doivent parfois être convertis en fraction 
+
+    """
     try:
         # Conversion directe
         result = float(value)
@@ -160,20 +163,31 @@ def string_to_float(value):
 
 
 
-import re 
-from unidecode import unidecode
-from conversions_unites import liquides_en_ml, solides_en_g
+def singulier(mot):
+    """
+    Met le mot au singulier 
+    """
+    return singularize(mot)
+
+
+
+##############################################################################################################################################
+
+## Fonctions de conversion d'une recette et calcul des calories de celle-ci ##
 
 def conversion_recette(recette):
     liste_ingredients = recette['Liste des ingrédients']           #Car recette est un dictionnaire de la forme {'recette' : """Titre""" , 'Liste des ingrédients' : ...}#
     """
-    Cette fonction prend la liste des ingredients retournée par le scraper de marmiton pour une recette donnée, et renvoie les ingrédients ainsi que leur conversion 
+    Cette fonction prend la liste des ingredients retournée par le scraper de marmiton pour une recette donnée, 
+    et renvoie les ingrédients ainsi que leur conversion dans l'unité interprétable par notre base de données (qui prend à la fois des ingrédients à 
+    l'unité : 1 oeuf, mais aussi des apports pour 100g ou 100ml)
 
     Args : 
-        liste_ingredients (list of str) : liste d'ingredients issus de la page marmiton de la recette 
+        recette (Dict) : dictionnaire avec le titre de la recette, la liste des ingrédients, ainsi que le lien de la recette 
 
     Returns : 
-        Dict : avec comme clé l'ingrédient et comme valeur (quantité,unité de mesure) ou (quantité)
+        nv_res (Dict): avec comme clé l'ingrédient et comme valeur (quantité,unité de mesure) ou (quantité)
+        fail (list) : liste contenant les ingrédients qui n'ont pas pu être matchés et interprétés par notre fonction
     
     """
     res = {} # dictionnaire resultat  
@@ -229,21 +243,17 @@ def conversion_recette(recette):
     return (nv_res,fail)
 
 
-from pattern.text.fr import singularize
-
-    def singulier(mot):
-        return singularize(mot)
-
-
-
-    def calcul_calories(recette,df):
+def calcul_calories(recette,df,vec):
     """
+    Cette fonction renvoie la valeur en kcal de la recette, cette dernière étant sous la forme : 
+    {'recette' : 'Titre recette' ,
+    'Liste ingrédients' : [(ingrédient1, quantité1 en g);(ingrédient2, quantité2 en g) ...] ,
+    'url':'url}
 
-    Args : recette, dataframe data_ciqual
-
-    Returns : 
-    
-    
+    Args : 
+        recette (Dict) : recette propre contenant les ingredients et leur quantité dans la bonne unité 
+        df (dataframe) : la base de données des ingrédients ciqual 
+        vec (vector) : la vectorisation des ingrédients 
     """
     calo = {}
     conv,fail = conversion_recette(recette) # renvoie un dictionnaire avec ingredient : qtte 
@@ -254,7 +264,7 @@ from pattern.text.fr import singularize
         print(*fail,sep=',')
     for (ingr,qtte) in conv.items(): 
         ingr = singulier(ingr)
-        match = find_match(ingr, df,vectors, 0.7) # on set le seuil à 0.7 et on trouve l'ingredient correpondant
+        match = find_match(ingr, df,vec, 0.7) # on set le seuil à 0.7 et on trouve l'ingredient correpondant
         if match == "Aucune correspondance trouvée":
             continue
         else:
